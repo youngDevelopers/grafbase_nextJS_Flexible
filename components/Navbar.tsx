e28d25cdba0c857e8cbcd36f6707f1150a306c50
youngDevelopers <dambaemmanuel5@gmail.com>
2023-06-29 16:32:21 +0200

    Added Header api-key to the actions

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,47 +1,49 @@
-import { NavLinks } from "@/constants"
+import { NavLinks } from "@/constants";
 import { getCurrentUser } from "@/lib/session";
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
 import AuthProviders from "./AuthProviders";
 
 const Navbar = async () => {
-    const session = await getCurrentUser();//login session
-    console.log(`session:${session}`)
-    return (
-        <nav className="flexBetween navbar">
-            <div className="flex-1 flexStart gap-10">
-                <Link href="/">
-                    <Image src="/logo.svg" width={115} height={43} alt="Flexible" />
-                </Link>
-                <ul className="hidden text-small gap-7 xl:flex">
-                    {
-                        NavLinks.map((link) => (
-                            <Link href={link.href} key={link.key}>
-                                {link.text}
-                            </Link>
-                        ))
-                    }
-                </ul>
-            </div>
-            <div className="flexCenter gap-4">
-                {
-                    session?.user ? (
-                        <>
-                            UserPhoto
+  const session = await getCurrentUser(); //login session
+  console.log(`session:${session}`);
+  return (
+    <nav className="flexBetween navbar">
+      <div className="flex-1 flexStart gap-10">
+        <Link href="/">
+          <Image src="/logo.svg" width={115} height={43} alt="Flexible" />
+        </Link>
+        <ul className="hidden text-small gap-7 xl:flex">
+          {NavLinks.map((link) => (
+            <Link href={link.href} key={link.key}>
+              {link.text}
+            </Link>
+          ))}
+        </ul>
+      </div>
+      <div className="flexCenter gap-4">
+        {session?.user ? (
+          <>
+            {session?.user?.image && (
+              <Image
+                src={session.user.image}
+                width={40}
+                height={40}
+                className="rounded-full"
+                alt={session.user.name}
+              />
+            )}
 
-                            <Link href="/create-project">
-                                Share Work
-                            </Link>
-                        </>
-                    ) : (
-                        <div>
-                            <AuthProviders />
-                        </div>
-                    )
-                }
-            </div>
-        </nav>
-    )
-}
+            <Link href="/create-project">Share Work</Link>
+          </>
+        ) : (
+          <div>
+            <AuthProviders />
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- lib/actions.ts
+++ lib/actions.ts
@@ -1,39 +1,45 @@
 import { createUserMutation, getUserQuery } from "@/graphql";
 import { GraphQLClient } from "graphql-request";
 
-const isProduction = process.env.NODE_ENV === 'production'; //setting variable on production level and localhost
+const isProduction = process.env.NODE_ENV === "production"; //setting variable on production level and localhost
 const apiUrl = isProduction
   ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || ""
   : "http://127.0.0.1:4000/graphql";
 
-const apiKey = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || "" : "letmein";
+const apiKey = isProduction
+  ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || ""
+  : "letmein";
 
-const serverUrl = isProduction ? process.env.NEXT_PUBLIC_SERVER_URL :"http://localhost:3000";
+const serverUrl = isProduction
+  ? process.env.NEXT_PUBLIC_SERVER_URL
+  : "http://localhost:3000";
 
 const client = new GraphQLClient(apiUrl);
 
-
 //makeGraphql request
 const makeGraphQLRequest = async (query: string, variables = {}) => {
-    try {
-        //client.request.....
-        return await client.request(query, variables);
-    } catch (error) {
-        throw error;
-    }
-}
-
+  try {
+    //client.request.....
+    return await client.request(query, variables);
+  } catch (error) {
+    throw error;
+  }
+};
 
 //get user by use of making a graphql request
 export const getUser = (email: string) => {
-    return makeGraphQLRequest(getUserQuery, { email })
-}
+  client.setHeader("x-api-key", apiKey);
+  return makeGraphQLRequest(getUserQuery, { email });
+};
 
-export const createUser = (name: string, email: string, avatarUrl: string ) => {
-    const variables = {
-        input: {
-            name, email, avatarUrl
-        }
-    };
-    return makeGraphQLRequest(createUserMutation, variables);
-}
+export const createUser = (name: string, email: string, avatarUrl: string) => {
+  client.setHeader("x-api-key", apiKey);
+  const variables = {
+    input: {
+      name,
+      email,
+      avatarUrl,
+    },
+  };
+  return makeGraphQLRequest(createUserMutation, variables);
+};
